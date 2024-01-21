06a09be2c4e38697e8a077c89e9be52d52f69b98
Azman Amin <azman@task.no>
2024-01-21 18:35:02 +0600

    audio recorder and player implementation

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Audi Recorder and Player
+
+## Setup and Run
+
+### Step 1: Browse to appropriate directory
+
+For server side solution browse to `/node-audio-recorder/server-side-solution`
+
+### Step 2: Install dependencies
+
+Run command `npm i`
+
+### Step 3: Run the application
+
+Run command `npm run dev`
+
+### Step 4: Open the application in the browser
+
+Visit `http://localhost:3001` for client-side-solution, and/or visit `http://localhost:3009` for the server-side-solution
+
+## How to Use
+
+1. Click on the `Record` button. (Don't release it!) It will listen for the audio.
+2. Say something to record.
+3. Release the button. It will save the record in the file system (under /audioFiles directory) and play the audio.
+4. Click `Play Audio` button if you want to listen to the latest recorded audio.
+
+> [!NOTE] > [Sox](https://sourceforge.net/projects/sox/) must be installed in your system for the `server-side-solution` to work properly.
---
+++ client-side-solution/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { writeFileSync, readFileSync } = require("fs");
+const { join } = require("path");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = createServer(app);
+const ioServer = new Server(server);
+const port = 3001;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+ioServer.on("connection", (socket) => {
+  socket.emit("connected", {
+    message: "Client is connected!",
+  });
+
+  socket.on("audioData", (data) => {
+    const dataURL = data.audio.dataURL.split(",").pop();
+
+    // Save the audio to a file
+    storeAudio(Buffer.from(dataURL, "base64"));
+
+    socket.emit("audioSaved", Buffer.from(dataURL, "base64"));
+  });
+
+  socket.on("playAudio", () => {
+    const filePath = join(__dirname, "audioFiles", "recorded_audio.wav");
+    const audioBuffer = readFileSync(filePath).buffer;
+
+    socket.emit("playAudio", audioBuffer);
+  });
+
+  const storeAudio = (audioBlob) => {
+    const filePath = join(__dirname, "audioFiles", "recorded_audio.wav");
+    writeFileSync(filePath, audioBlob);
+  };
+
+  // socket.on("audioData", (audioChunk) => {
+  //   const audioBlob = Buffer.concat(audioChunk);
+  //   storeAudio(audioBlob);
+
+  //   //   socket.emit("audioSaved", Buffer.from(audioBlob));
+  //   socket.emit("audioSaved", {
+  //     message: "Audio recorded and saved successfully.",
+  //     blob: audioBlob,
+  //   });
+  // });
+});
